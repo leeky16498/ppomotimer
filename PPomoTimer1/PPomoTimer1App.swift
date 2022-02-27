b4c5a523b29652d3a5a1cfd0088fe76c701d23fb
Kyungyun Lee, IOS Dev <leeky16498@gmail.com>
2022-02-27 19:23:28 +0000

    Initial Commit

---
+++ PPomoTimer1/PPomoTimer1App.swift
@@ -0,0 +1,17 @@
+//
+//  PPomoTimer1App.swift
+//  PPomoTimer1
+//
+//  Created by Kyungyun Lee on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PPomoTimer1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
